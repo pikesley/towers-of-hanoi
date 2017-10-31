1d8c0a0ca809927d138140ccae6821cb86cecd83
pikesley <github@orgraphone.org>
2017-10-31 22:49:38 +0000

    Can move discs

--- hanoi.py
+++ hanoi.py
@@ -1,50 +1,38 @@
-class Disc:
-    def __init__(self, size):
-        self.size = size
-        self.index = 0
-
-    def move(self):
-        if h[self.index][-1] == self:
-            h[self.index].pop()
-            self.index = (self.index + 1) % 3
-            h[self.index].append(self)
-
-class Pole(list):
-    def __init__(self, members = []):
-        for member in members:
-            self.append(member)
-
-    def display(self):
-        print map(lambda x: x.size, self)
+# class Disc:
+#     def __init__(self, size):
+#         self.size = size
+#         self.index = 0
+#
+#     def move(self):
+#         if h[self.index][-1] == self:
+#             h[self.index].pop()
+#             self.index = (self.index + 1) % 3
+#             h[self.index].append(self)
+#
+# class Pole(list):
+#     def __init__(self, members = []):
+#         for member in members:
+#             self.append(member)
+#
+#     def display(self):
+#         print map(lambda x: x.size, self)
 
 class Hanoi(list):
     def __init__(self, count):
-        self.discs = []
-        for i in range(count):
-            self.discs.append(Disc(i))
-
+        self.count = count
         for i in range(3):
-            self.append(Pole())
+            self.append([])
 
-        self[0] = Pole(members = self.discs[:])
-        self[0].reverse()
+        for i in range(self.count):
+            self[0] = [i] + self[0]
 
-    def display(self):
-        for pole in self:
-            pole.display()
+    def move(self, disc):
+        pole = 0
+        for i in range(self.count):
+            try:
+                self[i].index(disc)
+                pole = i
+            except ValueError:
+                pass
 
-if __name__ == '__main__':
-    h = Hanoi(2)
-    h.display()
-    print '---'
-
-    h.discs[0].move()
-    h.display()
-    print '---'
-
-    h.discs[0].move()
-    h.display()
-    print '---'
-
-    h.discs[0].move()
-    h.display()
+        self[(pole + 1) % 3].append(self[pole].pop())
---
+++ tests/test_hanoi.py
@@ -0,0 +1,19 @@
+from hanoi import Hanoi
+
+class TestHanoi:
+    def test_structure(self):
+        h = Hanoi(3)
+        assert h == [[2, 1, 0], [], []]
+
+    def test_move(self):
+        h = Hanoi(3)
+        h.move(0)
+        assert h == [[2, 1], [0], []]
+
+    def test_wrapping(self):
+        h = Hanoi(3)
+        h.move(0)
+        h.move(0)
+        h.move(0)
+
+        assert h == [[2, 1, 0], [], []]
