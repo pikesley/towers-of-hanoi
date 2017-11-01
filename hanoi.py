758aceb83fbca956990fc6e86ad50deb5661fe94
pikesley <github@orgraphone.org>
2017-11-01 19:58:32 +0000

    Now with output

--- hanoi.py
+++ hanoi.py
@@ -1,33 +1,53 @@
+import copy
 from indexer import Indexer
 
-class Hanoi(list):
+class Hanoi:
     def __init__(self, count):
+        self.indexer = Indexer(3)
         self.count = count
+        self.poles = []
         for i in range(3):
-            self.append([])
+            self.poles.append([])
 
         for i in range(self.count):
-            self[0] = [i] + self[0]
+            self.poles[0] = [i] + self.poles[0]
+
+        self.states = [copy.deepcopy(self.poles)]
+
+    def run(self):
+        count = 0
+        for index in self.indexer:
+            self.move(index)
+            count = count + 1
 
     def move(self, disc):
         pole = 0
         for i in range(self.count):
             try:
-                self[i].index(disc) == self.count
+                self.poles[i].index(disc) == self.count
                 pole = i
             except ValueError:
                 pass
 
-        mover = self[pole].pop()
-        if len(self[(pole + 1) % 3]) and self[(pole + 1) % 3][-1] < mover:
-            self[(pole + 2) % 3].append(mover)
+        mover = self.poles[pole].pop()
+        if len(self.poles[(pole + 1) % 3]) and self.poles[(pole + 1) % 3][-1] < mover:
+            self.poles[(pole + 2) % 3].append(mover)
         else:
-            self[(pole + 1) % 3].append(mover)
+            self.poles[(pole + 1) % 3].append(mover)
+
+        self.states.append(copy.deepcopy(self.poles))
+
+    def __str__(self):
+        s = ''
+        for state in self.states:
+            for pole in state:
+                s += str(pole)
+                s += "\n"
+            s += "------\n"
+
+        return s
 
 if __name__ == '__main__':
-    h = Hanoi(3)
-    for index in Indexer(4):
-        print h
-        h.move(index)
-
+    h = Hanoi(2)
+    h.run()
     print h
--- tests/test_hanoi.py
+++ tests/test_hanoi.py
@@ -3,25 +3,22 @@ from hanoi import Hanoi
 class TestHanoi:
     def test_structure(self):
         h = Hanoi(3)
-        assert h == [[2, 1, 0], [], []]
+        assert h.states[0] == [[2, 1, 0], [], []]
 
     def test_move(self):
         h = Hanoi(3)
         h.move(0)
-        assert h == [[2, 1], [0], []]
+        assert h.states[1] == [[2, 1], [0], []]
 
     def test_wrapping(self):
         h = Hanoi(3)
         h.move(0)
         h.move(0)
         h.move(0)
-
-        assert h == [[2, 1, 0], [], []]
+        assert h.states[3] == [[2, 1, 0], [], []]
 
     def test_stacking(self):
         h = Hanoi(3)
         h.move(0)
         h.move(1)
-
-        print h
-        assert h == [[2], [0], [1]]
+        assert h.states[2] == [[2], [0], [1]]
