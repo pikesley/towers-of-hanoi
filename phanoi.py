d0036de28f12d470e47b506b612ae66169f09044
Sam <sam@pikesley.org>
2017-11-18 22:42:45 +0000

    remove cruft

--- phanoi.py
+++ phanoi.py
@@ -5,38 +5,7 @@ from microdotphat import clear, set_pixel, show
 from towers import Towers
 
 INTERVAL = 0.3
-
-
-def phat_stacks(towers):
-    clear()
-
-    bit_offset = 0
-    toggle = 5
-    for bit in list(towers.binary):
-        digit(bit, 27 + bit_offset)
-        bit_offset = bit_offset + toggle
-        if toggle == 3:
-            toggle = 5
-        else:
-            toggle = 3
-
-    offset = 0
-    for stack in towers.stacks:
-        count = 0
-        for disc in stack:
-            shim = int((5 - (disc + 1)) / 2)
-            for i in range(disc + 1):
-                set_pixel(i + offset + shim, 6 - count, 1)
-            count = count + 1
-        offset = offset + 8
-        show()
-
-
-def digit(value, offset):
-    for i in range(3, 6):
-        if int(value) == 0:
-            set_pixel(offset, i, 1)
-        set_pixel(offset + 1, i, 1)
+DISCS = 5
 
 def display(towers):
     clear()
@@ -48,17 +17,8 @@ def display(towers):
         
 
 if __name__ == '__main__':
-    try:
-        discs = int(sys.argv[1])
-    except IndexError:
-        discs = 3
-    except ValueError:
-        discs = 3
-    if discs == 0:
-        discs = 3
-
     while True:
-        towers = Towers(discs)
+        towers = Towers(DISCS)
         display(towers)
         print towers.binary
         time.sleep(INTERVAL * 3)
