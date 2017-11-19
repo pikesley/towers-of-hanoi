d8d71666d3569c2404855b1e1a9f07b7365b5ab1
pikesley <github@orgraphone.org>
2017-11-19 18:54:51 +0000

    This is pretty ugly

--- webserver.py
+++ webserver.py
@@ -13,6 +13,9 @@ def lights():
     data = json.loads(request.data)['data']
     if RASPI:
         clear()
+    else:
+        s = ''
+
     for i in range(7):
         for j in range(45):
             if RASPI:
@@ -21,23 +24,20 @@ def lights():
                 except IndexError:
                     pass
             else:
-                s = ''
-                for i in range(7):
-                    for j in range(45):
-                        bit = data[i][j]
-                        if (4 - (j % 8)) < 0:
-                            s += ' '
-                        else:
-                            if bit == 0:
-                                s += '.'
-                            if bit == 1:
-                                s += 'o'
-
-                    s += "\n"
-                print s                
+                bit = data[i][j]
+                if (4 - (j % 8)) < 0:
+                    s += ' '
+                else:
+                    if bit == 0:
+                        s += '.'
+                    if bit == 1:
+                        s += 'o'
+        s += "\n"
 
     if RASPI:
         show()
+    else:
+        print s
 
     return json.dumps({
             'success': True
