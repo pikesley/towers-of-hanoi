8e85ac895157fae79764a5089ad94cd44a6e5b19
pikesley <github@orgraphone.org>
2017-11-19 13:53:27 +0000

    Got most of this now

--- lib/towers.rb
+++ lib/towers.rb
@@ -10,12 +10,17 @@ class Towers
 
   def move
     flip = Towers.diff Towers.binarise(@count, @discs), Towers.binarise(@count += 1, @discs)
+    source = Towers.find_disc flip, @stacks
 
-    @stacks[1].push @stacks[0].pop
+    @stacks[Towers.find_stack flip, source, @stacks].push @stacks[source].pop
   end
 
   def binary
     Towers.binarise @count, @discs
+  end
+
+  def solved
+    binary.chars.all? { |bit| bit.to_i == 1 }
   end
 
   def Towers.binarise value, width
@@ -28,5 +33,23 @@ class Towers
         return index
       end
     end
+  end
+
+  def Towers.find_disc needle, stacks
+    stacks.each_with_index do |stack, index|
+      return index if stack.index needle
+    end
+  end
+
+  def Towers.find_stack disc, source, stacks
+    if stacks[(source + 1) % 3] == []
+      return (source + 1) % 3
+    end
+
+    if stacks[(source + 1) % 3][-1] < disc
+      return (source + 2) % 3
+    end
+
+    return (source + 1) % 3
   end
 end
